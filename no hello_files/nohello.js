14d9588256e185fcb4fa1fcc57fdc796395cc4f1
Marcell Almeida <almeidamarcell@gmail.com>
2022-12-04 15:31:53 -0300

    Add files via upload

---
+++ no hello_files/nohello.js
@@ -0,0 +1,27 @@
+var typed2 = new Typed('#strike', {
+  strings: [
+    'Oi, tudo bem?',
+    'como vai?',
+    'bom dia!',
+    'boa tarde',
+    'namaste',
+    'Boa noite!',
+    'olá',
+    'tá ai?',
+    'tudo bem?',
+  ],
+  typeSpeed: 80,
+  backSpeed: 60,
+  smartBackspace: false,
+  loop: true,
+  shuffle: false,
+  backDelay: 2000,
+  startDelay: 3000,
+});
+
+// force the start of cursor animation while the `startDelay` is ticking
+if (typed2.cursor != null) {
+  // can't use `toggleBlinking(true)` here, as it has some extra checks
+  // whether animation has started...which defeats the purpose
+  typed2.cursor.classList.add('typed-cursor--blink');
+}
